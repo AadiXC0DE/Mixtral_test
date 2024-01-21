201def8fe952ae7cb6faf00c442bac6686b31b17
AadiXC0DE <aadityaz2077@gmail.com>
2024-01-21 19:10:23 +0530

    fix styles

--- src/app/page.js
+++ src/app/page.js
@@ -95,9 +95,7 @@ export default function Home() {
         return;
       }
 
-
       const base64Data = croppedImage.split(",")[1];
-
 
       const response = await axios.post(
         "https://swa3p4ickqt523o7c3am5tdege0iplck.lambda-url.us-east-1.on.aws/",
@@ -114,7 +112,6 @@ export default function Home() {
       console.log(response.data);
       setChatResponse(response.data);
 
-
       setImage(null);
     } catch (error) {
       console.error("Error calling the API:", error);
@@ -122,87 +119,93 @@ export default function Home() {
   };
 
   return (
-    <div className="bg-gray-100">
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold text-black mb-4">Doubt Solver</h1>
+    <div className="bg-gray-100 h-screen">
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold text-black mb-4">Doubt Solver</h1>
 
-      <div className="flex flex-wrap mb-4">
-        <input
-          type="text"
-          value={input}
-          onChange={handleInputChange}
-          className="border bg-yellow-100 text-black p-2 flex-grow mb-2 sm:mr-2 sm:mb-0"
-          placeholder="Type your question..."
-        />
-        <label className="relative cursor-pointer ml-2 bg-yellow-300 text-black p-2 rounded mb-2 sm:mb-0 flex items-center">
+        <div className="flex flex-wrap mb-4">
           <input
-            type="file"
-            onChange={handleImageChange}
-            accept="image/*"
-            className="hidden"
+            type="text"
+            value={input}
+            onChange={handleInputChange}
+            className="border bg-yellow-100 text-black p-2 flex-grow mb-2 sm:mr-2 sm:mb-0"
+            placeholder="Type your question..."
           />
-          <CameraIcon className="h-5 w-5 mr-2" />
-          Upload
-        </label>
-        <button
-          onClick={handleSubmit}
-          className="bg-yellow-300 text-black ml-2 p-2 rounded mb-2 sm:ml-2 sm:mb-0"
-        >
-          Submit
-        </button>
-        <button
-          onClick={handleImageSend}
-          className="bg-yellow-300 text-black ml-2 p-2 rounded mb-2 sm:ml-2 sm:mb-0"
-        >
-          Submit Image
-        </button>
+          <label className="relative cursor-pointer ml-2 bg-yellow-300 text-black p-2 rounded mb-2 sm:mb-0 flex items-center">
+            <input
+              type="file"
+              onChange={handleImageChange}
+              accept="image/*"
+              className="hidden"
+            />
+            <CameraIcon className="h-5 w-5 mr-2" />
+            Upload
+          </label>
+          <button
+            onClick={handleSubmit}
+            className="bg-yellow-300 text-black ml-2 p-2 rounded mb-2 sm:ml-2 sm:mb-0"
+          >
+            Submit
+          </button>
+          <button
+            onClick={handleImageSend}
+            className="bg-yellow-300 text-black ml-2 p-2 rounded mb-2 sm:ml-2 sm:mb-0"
+          >
+            Submit Image
+          </button>
+        </div>
+
+        {image && (
+          <div className="mb-4">
+            {image && (
+              <div>
+                <Cropper
+                  src={image}
+                  style={{ height: 400, width: "100%" }}
+                  initialAspectRatio={16 / 9}
+                  guides={true}
+                  crop={onCrop}
+                  ref={cropperRef}
+                />
+                <div className="flex justify-center">
+                  {" "}
+                  <button
+                    onClick={handleCrop}
+                    className="bg-white mt-2 w-36 font-bold text-lg text-black cursor-pointer hover:bg-blue-400 hover:text-white p-2 rounded"
+                  >
+                    Crop
+                  </button>
+                </div>
+              </div>
+            )}
+
+            {croppedImage && (
+              <div>
+                <h3 className="text-xl mb-2">Cropped Image Preview</h3>
+                <Image
+                  width={300}
+                  height={200}
+                  src={croppedImage}
+                  alt="Cropped"
+                  style={{ maxWidth: "100%", maxHeight: 400 }}
+                />
+
+                <canvas
+                  ref={previewCanvasRef}
+                  width={300}
+                  height={200}
+                ></canvas>
+              </div>
+            )}
+          </div>
+        )}
+
+        {chatResponse && (
+          <div className="bg-yellow-50 p-4 rounded text-black text-md text-medium">
+            <Latex>{chatResponse}</Latex>
+          </div>
+        )}
       </div>
-
-      {image && (
-        <div className="mb-4">
-          {image && (
-            <div>
-              <Cropper
-                src={image}
-                style={{ height: 400, width: "100%" }}
-                initialAspectRatio={16 / 9}
-                guides={true}
-                crop={onCrop}
-                ref={cropperRef}
-              />
-              <div className="flex justify-center"> <button
-                onClick={handleCrop}
-                className="bg-white mt-2 w-36 font-bold text-lg text-black cursor-pointer hover:bg-blue-400 hover:text-white p-2 rounded"
-              >
-                Crop
-              </button></div>
-
-            </div>
-          )}
-
-          {croppedImage && (
-            <div>
-              <h3 className="text-xl mb-2">Cropped Image Preview</h3>
-              <Image
-                width={300}
-                height={200}
-                src={croppedImage}
-                alt="Cropped"
-                style={{ maxWidth: "100%", maxHeight: 400 }}
-              />
-
-              <canvas ref={previewCanvasRef} width={300} height={200}></canvas>
-            </div>
-          )}
-        </div>
-      )}
-
-      {chatResponse && (
-        <div className="bg-yellow-50 p-4 rounded text-black text-md text-medium">
-          <Latex>{chatResponse}</Latex>
-        </div>
-      )}
-    </div>
     </div>
   );
 }
